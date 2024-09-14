e21aad53b3eafef7ef24aa3cf309dd55d848692b
Harit Thoriya <thoriyaharit@gmail.com>
2024-09-14 03:26:53 -0400

    Initial commit

---
+++ src/test/kotlin/com/harit/springsecurityjwt/SpringSecurityJwtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.harit.springsecurityjwt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
